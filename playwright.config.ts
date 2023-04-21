daa5e5ca810b30d9cf87a6b5c542bce597308ee7
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2023-04-21 10:35:05 +0300

    upload report to s3

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,10 +16,9 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     retries: 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 1,
+    workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env.CI ? 'html' : 'line',
+    reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
